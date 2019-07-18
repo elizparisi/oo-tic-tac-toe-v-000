7b61524aea77a2075e36e3358bb92540211495db
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-07-18 01:49:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,24 +51,23 @@ class TicTacToe
     @board.count { |token| token == "X" || token == "O"}
   end
   
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
+    def current_player
+    turn_count.even? ? "X" : "O"
   end
-  
+
   def turn
     puts "Please enter a number (1-9):"
     user_input = gets.strip
     index = input_to_index(user_input)
-  
     if valid_move?(index)
       token = current_player
       move(index, token)
     else
       turn
     end
-      display_board
+    display_board
   end
-  
+
   def won?
     WIN_COMBINATIONS.any? do |combo|
       if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
@@ -76,28 +75,27 @@ class TicTacToe
       end
     end
   end
-  
+
   def full?
-    @board.all?{|space| space != " " }
+    @board.all?{|square| square != " " }
   end
-  
+
   def draw?
-    !won? && board.full?
+    full? && !won?
   end
-  
-  def over? 
-    draw? || won?
+
+  def over?
+    won? || draw?
   end
-  
-  def winner 
+
+  def winner
     if combo = won?
       @board[combo[0]]
     end
   end
-  
-  def play 
+
+  def play
     turn until over?
     puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
-  end 
+  end
 end
-
